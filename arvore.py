1d7b779994ff4121f40a138ed02149469e805255
Ana Carvalho <anabeatriz0242@gmail.com>
2023-10-07 20:12:12 +0100

    commit

--- arvore.py
+++ arvore.py
@@ -1,60 +1,54 @@
 #abrem o anaconda powershell e depois colem isto:  conda init powershell
+class NoArvore: #esta classe representa a arvore; e um no individual da arvore
+    def __init__(self, valor): 
+      self.valor = valor 
+      self.filhos = [] #numero de filhos
+      self.pai = None
 
-class NoArvore: #esta classe representa o primeiro no
-    def __init__(self, valor): #a funcao __init__ serve para iniciar classes e dar valores àS propriedades de um objeto (self)
-        self.esquerda = None
-        self.direita = None
-        self.valor = valor
+    def adicionar_filho(self, filho): #adicionar um filho ao objeto self, tornando o self o pai
+        filho.pai = self
+        self.filhos.append(filho)
 
-#arvore = NoArvore(2)
-#arvore.esquerda = NoArvore(3)
-#arvore.direita = NoArvore(1)
-#arvore.direita.direita = NoArvore(7)
+    def procurar_nivel(self): #conta se o numero de antepassados (pais anteriores)
+        nivel = 0 #o no raiz esta no nivel 0
+        p = self.pai
+        while p: #enquanto existir pai incrementa-se um no nivel
+           nivel += 1
+           p = p.pai
+        return nivel
 
-#a funcao aseguir e a versao automatizada do codigo 4 linhas anteriores
-
-    def inserir(self, valor): #se o novo no tiver um valor menor que o valor anterior vai para a esquerda, e se for maior vai para a direita
-        if valor < self.valor:
-             if self.esquerda is None:
-                  self.esquerda = NoArvore(valor)
-             else:
-                  self.esquerda.inserir(valor)
-        else:
-             if self.direita is None:
-                  self.direita = NoArvore(valor)
-             else:
-                  self.direita.inserir(valor)
-
-
-
-    #podemos percorrer uma arvore por 3 formas: pre-ordem, em ordem, e pos-ordem; o em ordem e mais adequado 
-
-    def travessia_em_ordem(self): #imprime primeiro o no que se encontra mais a esquerda e depois percorre os anteriores e imprime o que se encontra mais a esquerda primeiro; se nao entenderem depois explico vos melhor
-         if self.esquerda:
-            self.esquerda.travessia_em_ordem()
-         print(self.valor)
-         if self.direita:
-            self.direita.travessia_em_ordem()
-     
+    def mostrar_arvore(self): #funcao para imprimir a arvore
+      espacos = ' ' * self.procurar_nivel() * 3 #isto inicializa um espaco a cada nivel; nivel 1 com 1 espaco, 2 com 2 e etc..
+      ramo = espacos + "|_" if self.pai else "" #se existir pai
+      print(ramo + self.valor) #imprime o pai
+      if len(self.filhos) > 0: #se houver filhos
+        for filho in self.filhos:
+            filho.mostrar_arvore() #imprime os valores dos seguintes filhos
 
 
+def build_product_tree():
+    raiz = NoArvore("Electronics")
 
+    laptop = NoArvore("Laptop")
+    laptop.adicionar_filho(NoArvore("Mac"))
+    laptop.adicionar_filho(NoArvore("Surface"))
+    laptop.adicionar_filho(NoArvore("Thinkpad"))
 
+    cellphone = NoArvore("Cell Phone")
+    cellphone.adicionar_filho(NoArvore("iPhone"))
+    cellphone.adicionar_filho(NoArvore("Google Pixel"))
+    cellphone.adicionar_filho(NoArvore("Vivo"))
 
+    tv = NoArvore("TV")
+    tv.adicionar_filho(NoArvore("Samsung"))
+    tv.adicionar_filho(NoArvore("LG"))
 
+    raiz.adicionar_filho(laptop)
+    raiz.adicionar_filho(cellphone)
+    raiz.adicionar_filho(tv)
 
+    raiz.mostrar_arvore()
 
-  #def add_child(self, child): #adicionar um child ao objeto self, tornando o self o parent
-        #child.parent = self
-        #self.children.append(child)
+if __name__ == '__main__':
+    build_product_tree()
 
-arvore = NoArvore(10)
-arvore.inserir(4)
-arvore.inserir(2)
-arvore.inserir(9)
-arvore.inserir(6)
-arvore.inserir(3)
-arvore.inserir(45)
-arvore.inserir(56)
-
-arvore.travessia_em_ordem()
