06088c302f2d0ae87e28f2b0781d3757cec4c5cb
Ana Carvalho <anabeatriz0242@gmail.com>
2023-10-09 16:07:12 +0100

    commit

--- arvore.py
+++ arvore.py
@@ -1,5 +1,5 @@
 #abrem o anaconda powershell e depois colem isto:  conda init powershell
-class NoArvore: #esta classe representa a arvore; e um no individual da arvore
+class NoArvore: #esta classe representa a arvore; um no individual da arvore
     def __init__(self, valor): 
       self.valor = valor 
       self.filhos = [] #numero de filhos
@@ -12,24 +12,24 @@ class NoArvore: #esta classe representa a arvore; e um no individual da arvore
     def procurar_nivel(self): #conta se o numero de antepassados (pais anteriores)
         nivel = 0 #o no raiz esta no nivel 0
         p = self.pai
-        while p: #enquanto existir pai incrementa-se um no nivel
-           nivel += 1
+        while p: #enquanto existir pai incrementa-se um nivel
+           nivel += 1 
            p = p.pai
         return nivel
 
     def mostrar_arvore(self): #funcao para imprimir a arvore
-      espacos = ' ' * self.procurar_nivel() * 3 #isto inicializa um espaco a cada nivel; nivel 1 com 1 espaco, 2 com 2 e etc..
+      espacos = ' ' * self.procurar_nivel()  #isto inicializa um espaco a cada nivel; nivel 1 com 1 espaco, 2 com 2 e etc..
       ramo = espacos + "|_" if self.pai else "" #se existir pai
-      print(ramo + self.valor) #imprime o pai
+      print(ramo + str(self.valor)) #imprime o pai
       if len(self.filhos) > 0: #se houver filhos
         for filho in self.filhos:
             filho.mostrar_arvore() #imprime os valores dos seguintes filhos
 
 
-def build_product_tree():
-    raiz = NoArvore("Electronics")
+def construir_arvore():
+    raiz = NoArvore(1)
 
-    laptop = NoArvore("Laptop")
+    laptop = NoArvore(2)
     laptop.adicionar_filho(NoArvore("Mac"))
     laptop.adicionar_filho(NoArvore("Surface"))
     laptop.adicionar_filho(NoArvore("Thinkpad"))
@@ -50,5 +50,5 @@ def build_product_tree():
     raiz.mostrar_arvore()
 
 if __name__ == '__main__':
-    build_product_tree()
+    construir_arvore()
 
