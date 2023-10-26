85db3736e341d6b9aec5bbc6b626ce69bf3eb04f
Ana Carvalho <anabeatriz0242@gmail.com>
2023-10-26 12:11:05 +0100

    main

--- gerador_matriz.py
+++ gerador_matriz.py
@@ -1,17 +1,8 @@
 import numpy as np
 import pandas as pd
 import xlsxwriter
-import openpyxl
-
 
 
 #criar matriz aleatória com 0 e 1
-#A = np.random.randint(0, 2, (8, 8))
-#print(A)
-
-#escrever matriz em excel
-#df = pd.DataFrame(A).T
-#df.to_excel(excel_writer = "matriz.xlsx")
-
-df = pd.read_excel("matriz.xlsx")
-df.as_matrix()
+A = np.random.randint(0, 2, (25, 25))
+print(A)
