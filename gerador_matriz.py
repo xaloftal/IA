d6a95fb67b0850ee135a2bcde8b4f1f2dffa98dc
xaloftal <dianaacdinis@gmail.com>
2023-10-26 12:10:18 +0100

    random matrix com probabilidades

--- gerador_matriz.py
+++ gerador_matriz.py
@@ -9,9 +9,12 @@ import openpyxl
 #A = np.random.randint(0, 2, (8, 8))
 #print(A)
 
+A = np.random.choice([0, 1], size=(25,25), p=[.7, .3])
+print(A)
+
 #escrever matriz em excel
 #df = pd.DataFrame(A).T
 #df.to_excel(excel_writer = "matriz.xlsx")
 
-df = pd.read_excel("matriz.xlsx")
-df.as_matrix()
+# df = pd.read_excel("matriz.xlsx")
+# df.as_matrix()
