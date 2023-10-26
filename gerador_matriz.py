98a5fc3f8867117525e3ac2c71df0d9acc8bdb9e
Ana Carvalho <anabeatriz0242@gmail.com>
2023-10-26 11:58:46 +0100

    main

--- gerador_matriz.py
+++ gerador_matriz.py
@@ -4,9 +4,12 @@ import xlsxwriter
 
 
 #criar matriz aleatória com 0 e 1
-A = np.random.randint(0, 2, (8, 8))
-print(A)
+#A = np.random.randint(0, 2, (8, 8))
+#print(A)
 
 #escrever matriz em excel
-df = pd.DataFrame(A).T
-df.to_excel(excel_writer = "matriz.xlsx")
+#df = pd.DataFrame(A).T
+#df.to_excel(excel_writer = "matriz.xlsx")
+
+df = pd.read_excel("matriz.xlsx")
+df.as_matrix()
