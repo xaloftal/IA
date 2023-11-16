39a1c2a8e2fe772ad61864578d4deb8a78841dc0
Ana Carvalho <anabeatriz0242@gmail.com>
2023-11-16 11:39:36 +0000

    a*

--- Aestrela.py
+++ Aestrela.py
@@ -1,35 +1,38 @@
 from matrizes import *
 import heapq
+import pygame
+import sys
+import time
 
 class Node:
-    def __init__(self, x, y, parent=None, g=0, h=0): #inicializa o no com as suas coordenadas, o no parente, o custo do inicio ate este no e o custo heuristico(custo do no ate ao objetivo)
+    def __init__(self, x, y, parent=None, g=0, h=0): # inicializa o no com as suas coordenadas, o no parente, o custo do inicio ate este no e o custo heuristico(custo do no ate ao objetivo)
 
-        self.x = x #coordenada x do nó
-        self.y = y #coordenada y do nó
-        self.parent = parent #no parente
-        self.g = g  #custo do inicio ate este no
+        self.x = x # coordenada x do nó
+        self.y = y # coordenada y do nó
+        self.parent = parent # no parente
+        self.g = g  # custo do inicio ate este no
         self.h = h  # heuristico (estimado) deste no ate ao objetivo
 
-    def total_cost(self): #retorna a soma de g (custo do inicio ate este no) e h (heuristico (estimado) deste no ate ao objetivo)
+    def total_cost(self): # retorna a soma de g (custo do inicio ate este no) e h (heuristico (estimado) deste no ate ao objetivo)
 
         return self.g + self.h
 
     def __lt__(self, other): #compara dois nos baseados no seu custo total
 
-        #o objeto other e o outro no
-        #retorna TRUE se este no tiver um custo total inferior ao outro no
+        # o objeto other e o outro no
+        # retorna TRUE se este no tiver um custo total inferior ao outro no
 
         return self.total_cost() < other.total_cost()
 
-def heuristic(point, goal): #calcula e retorna o custo heuristico (estimado) de uma certa coordenada até ao objetivo (distância de Manhattan)
+def heuristic(point, goal): # calcula e retorna o custo heuristico (estimado) de uma certa coordenada até ao objetivo (distância de Manhattan)
 
-    #point representa as coordenadas atuais; goal representa as coordenadas do objetivo
+    # point representa as coordenadas atuais; goal representa as coordenadas do objetivo
 
     return abs(point[0] - goal[0]) + abs(point[1] - goal[1])
 
-def get_neighbors(matrix, node): #retorna a lista de nos vizinhos que podem ser atingidos pelo no atual
+def get_neighbors(matrix, node): # retorna a lista de nos vizinhos que podem ser atingidos pelo no atual
 
-    #matrix representa a matriz; node representa o no atual
+    # matrix representa a matriz; node representa o no atual
 
     neighbors = []
     x, y = node.x, node.y
@@ -40,7 +43,7 @@ def get_neighbors(matrix, node): #retorna a lista de nos vizinhos que podem ser 
             neighbors.append(Node(new_x, new_y))
     return neighbors
 
-def construct_path(goal): #retorna o caminho encontrado
+def construct_path(goal): # retorna o caminho encontrado
 
     path = []
     while goal:
@@ -48,10 +51,10 @@ def construct_path(goal): #retorna o caminho encontrado
         goal = goal.parent
     return path[::-1]  # reverte o caminho, para listar as coordenadas do start ate ao goal
 
-def astar(matrix, start, goal): #realiza o algoritmo A* para encontrar o caminho mais curto; retorna a lista de coordenadas que representam o caminho ou nada se não houver caminho
+def astar(matrix, start, goal): # realiza o algoritmo A* para encontrar o caminho mais curto; retorna a lista de coordenadas que representam o caminho ou nada se não houver caminho
 
-    open_set = []   #numero de nos a serem avaliados
-    closed_set = set()  #nos ja avaliados
+    open_set = []   # numero de nos a serem avaliados
+    closed_set = set()  # nos ja avaliados
 
     start_node = Node(start[0], start[1], None, 0, heuristic(start, goal)) 
     heapq.heappush(open_set, start_node)
@@ -60,15 +63,15 @@ def astar(matrix, start, goal): #realiza o algoritmo A* para encontrar o caminho
         current_node = heapq.heappop(open_set)
 
         if (current_node.x, current_node.y) == goal:
-            return construct_path(current_node)  #se o objetivo for atingido, retorna o caminho
+            return construct_path(current_node)  # se o objetivo for atingido, retorna o caminho
 
-        closed_set.add((current_node.x, current_node.y))  # Mark the current node as evaluated.
+        closed_set.add((current_node.x, current_node.y))  # marca o no atual como avaliado
 
         for neighbor in get_neighbors(matrix, current_node):
             if (neighbor.x, neighbor.y) in closed_set:
-                continue  # Skip nodes already evaluated.
+                continue  # salta nos ja avaliados.
 
-            tentative_g = current_node.g + 1  # Assuming a cost of 1 to move to a neighbor.
+            tentative_g = current_node.g + 1  # assumo o custo de um a ir para um vizinho.
 
             if neighbor not in open_set or tentative_g < neighbor.g:
                 neighbor.g = tentative_g
@@ -78,15 +81,68 @@ def astar(matrix, start, goal): #realiza o algoritmo A* para encontrar o caminho
                 if neighbor not in open_set:
                     heapq.heappush(open_set, neighbor)
 
-    return None  # No path found
+    return None  # nenhum caminho encontrado
 
-# Example usage
+# Pygame initialization
+pygame.init()
 
-start = (0, 0)
-goal = (4, 4)
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
-path = astar(matrix2530, start, goal)
-if path:
-    print("Path found:", path)
-else:
-    print("No path found")
+# Cell size
+CELL_SIZE = 20
+
+def draw_grid(matrix, screen):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            color = WHITE if matrix[row][col] == 0 else BLACK
+            pygame.draw.rect(screen, color, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 0)
+
+def draw_path(path, screen):
+    for node in path:
+        pygame.draw.rect(screen, GREEN, (node[1] * CELL_SIZE, node[0] * CELL_SIZE, CELL_SIZE, CELL_SIZE), 0)
+        pygame.display.update()
+        time.sleep(0.2)
+
+def main():
+    matrix = [
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0]
+    ]
+    
+    start = (0, 0)
+    goal = (4, 4)
+
+    screen_size = (len(matrix[0]) * CELL_SIZE, len(matrix) * CELL_SIZE)
+    screen = pygame.display.set_mode(screen_size)
+    pygame.display.set_caption("A* Algorithm Visualization")
+
+    running = True
+
+    # A* algorithm
+    path = astar(matrix, start, goal)
+    if path:
+        print("Path found:", path)
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            screen.fill(WHITE)
+            draw_grid(matrix, screen)
+            draw_path(path, screen)
+
+            pygame.display.flip()  # Update the display
+
+        pygame.quit()
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
