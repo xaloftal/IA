cc2afa0c0e6f901a98dd18414b8b34cc70f70a40
xaloftal <dianaacdinis@gmail.com>
2023-12-04 16:34:42 +0000

    trabalho de relatório

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from matriz30 import *
 from matriz50 import *
+from matriz45 import *
 from bfs import *
 from dfs import *
 from aStar import *
@@ -35,15 +36,16 @@ def show_menu_matrices():
         print("5. matriz 25030")
         print("6. matriz 30030")
         print("7. matriz 40030")
-        print("8. matriz 50030")
-        print("9. matriz 2550")
-        print("10. matriz 5050")
-        print("11. matriz 10050")
-        print("12. matriz 25050")
-        print("13. matriz 30050")
-        print("14. matriz 40050")
-        print("15. matriz 50050")
-        print("16. Voltar")
+        print("8. matriz 50030\n")
+        print("9. matriz 1045")
+        print("10. matriz 2545")
+        print("11. matriz 5045")
+        print("12. matriz 10045")
+        print("13. matriz 25050")
+        print("14. matriz 30050")
+        print("15. matriz 40050")
+        print("16. matriz 50050")
+        print("17. Voltar")
 
         choice_matrices = input("\nEscolha uma opção: ")
 
@@ -63,21 +65,25 @@ def show_menu_matrices():
             return matrix40030
         elif choice_matrices == '8':
             return matrix50030
+        
         elif choice_matrices == '9':
-            return matrix2550
+            return matrix1045
         elif choice_matrices == '10':
-            return matrix5050
+            return matrix2545
         elif choice_matrices == '11':
-            return matrix10050
+            return matrix5045        
         elif choice_matrices == '12':
-            return matrix25050
+            return matrix10045
+        
         elif choice_matrices == '13':
-            return matrix30050
+            return matrix25050
         elif choice_matrices == '14':
-            return matrix40050
+            return matrix30050
         elif choice_matrices == '15':
-            return matrix50050
+            return matrix40050
         elif choice_matrices == '16':
+            return matrix50050
+        elif choice_matrices == '17':
             return None
         else:
             print("Opção inválida. Tente novamente.")
